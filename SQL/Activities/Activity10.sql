0a9e2d9a4b3d394ac7b98b45a62f07b3449e1527
ashokkuppan <106366283+ashokkuppan@users.noreply.github.com>
2022-07-08 08:19:40 +0530

    SQL Activities

---
+++ SQL/Activities/Activity10.sql
@@ -0,0 +1,19 @@
+REM   Script: Activity10
+REM   Activity10
+
+
+SELECT * FROM orders 
+WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE city='Paris');
+
+select * from salesman;
+
+SELECT * FROM orders 
+WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE SALESMAN_CITY='Paris');
+
+SELECT grade, COUNT(*) FROM customers 
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city='New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders 
+WHERE salesman_id IN( SELECT salesman_id FROM salesman 
+WHERE commission=( SELECT MAX(commission) FROM salesman));
+
